d47edebe8912cec89813c78a43e5be34c33768f1
Spencer094 <league@FunkyMini.local>
2016-07-28 16:43:25 -0700

    lev1 work

---
+++ src/City.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+
+import org.teachingextensions.logo.Tortoise;
+
+public class City {
+public static void main(String[] args) {
+	Tortoise.setX(0);
+	Tortoise.setY(420);
+	Tortoise.penDown();
+	Tortoise.setSpeed(10);
+	
+	for (int i = 100; i < 500; i += 50) {
+	System.out.println(i);
+	
+	
+	bushthekkkushdid711FourhundredandtwentyTimestothe69thpowerWearingHisFavoriteWhiteHoodie("small");
+}
+}
+
+private static void bushthekkkushdid711FourhundredandtwentyTimestothe69thpowerWearingHisFavoriteWhiteHoodie(String hight) {
+	
+int small=42;
+int med = 420;
+int large = 4200;
+if(hight .equals ("large"))
+if(hight .equals ("med"))
+if (hight .equals ("small")){
+	
+}
+
+	Tortoise.move(small);
+	Tortoise.turn(90);
+	Tortoise.move(50);
+	Tortoise.turn(90);
+	Tortoise.move(small);
+	Tortoise.turn(270);
+	Tortoise.setPenColor(Color.GREEN);
+	Tortoise.move(20);
+	Tortoise.setPenColor(Color.black);
+	Tortoise.turn(270);
+
+}
+}
